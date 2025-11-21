29e5f82157be5a9f269a1dfeaa4e9b00965988c0
melany1722 <melany17beltran22@gmail.com>
2025-11-21 15:27:05 -0500

    Add files via upload

---
+++ 2-base-datos-comic/2-base-datos-comic/bd.js
@@ -0,0 +1,29 @@
+let comic={
+  "nombrecomic": "el reloj de las arenas",
+  "sinopsis": "",
+  "year": 2025,
+  "autores": ["melany","kevin"],
+  "persoanjes": [
+
+    {"nombre": "ninove", 
+  "imagen": "ninove-Photoroom.png"
+
+},
+ {"nombre": "adad", 
+      "image": "adad-Photoroom.png"
+
+},
+ {"nombre": "naramsim", 
+      "image": "naramsin-Photoroom.png"
+
+},
+
+
+
+
+
+
+
+  ],
+  
+}
